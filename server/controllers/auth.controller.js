b64df3d713bc188460712d35a30063f74917f5c9
Eslam Kamal <ek191744@gmail.com>
2025-04-21 19:02:43 +0200

    Added login endpoint

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -79,7 +79,37 @@ export const verifyEmail = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-    res.send("Login route");
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid email",
+            });
+        }
+
+        const isPasswordValid = await bcryptjs.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid password",
+            });
+        }
+
+        generateTokenAndSetCookie(res, user._id);
+
+        user.lastLogin = new Date();
+        await user.save();
+
+        res.status(200).json({
+            success: true,
+            message: "Logged in successfully",
+            user: { ...user._doc, password: undefined },
+        });
+    } catch (err) {
+        res.status(400).json({ success: false, message: err.message });
+    }
 };
 
 export const logout = async (req, res) => {
