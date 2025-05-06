d2bb2047dd89516fa764b366c569484cb12c44d5
Eslam Kamal <ek191744@gmail.com>
2025-05-06 14:52:44 +0300

    Add Check Auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,42 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import FloatingShape from "./components/FloatingShape";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import EmailVerificationPage from "./pages/EmailVerificationPage";
 import { Toaster } from "react-hot-toast";
+import { useAuthStore } from "./store/authStore";
+import { useEffect } from "react";
+import DashboardPage from "./pages/DashboardPage";
+
+const ProtectedRoute = ({ children }) => {
+    const { isAuthenticated, user } = useAuthStore();
+    if (!isAuthenticated) {
+        return <Navigate to="/login" replace />;
+    }
+    if (!user.isVerified) {
+        return <Navigate to="/verify-email" replace />;
+    }
+    return children;
+};
+
+// This component redirects authenticated and verified users to the home page.
+const RedirectAuthenticatedUser = ({ children }) => {
+    const { isAuthenticated, user } = useAuthStore();
+    if (isAuthenticated && user.isVerified) {
+        return <Navigate to="/" replace />;
+    }
+    return children;
+};
 
 function App() {
+    const { isCheckingAuth, checkAuth, isAuthenticated, user } = useAuthStore();
+
+    useEffect(() => {
+        checkAuth();
+    }, [checkAuth]);
+
+    console.log("isAuthenticated", isAuthenticated);
+    console.log("user", user);
     return (
         <div className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden">
             <FloatingShape
@@ -30,10 +61,34 @@ function App() {
                 delay={2}
             ></FloatingShape>
             <Routes>
-                <Route path="/" element={"Home"}></Route>
-                <Route path="/signup" element={<SignUpPage/>}></Route>
-                <Route path="/login" element={<LoginPage/>}></Route>
-                <Route path="/verify-email" element={<EmailVerificationPage/>}></Route>
+                <Route
+                    path="/"
+                    element={
+                        <ProtectedRoute>
+                            <DashboardPage />
+                        </ProtectedRoute>
+                    }
+                ></Route>
+                <Route
+                    path="/signup"
+                    element={
+                        <RedirectAuthenticatedUser>
+                            <SignUpPage />
+                        </RedirectAuthenticatedUser>
+                    }
+                ></Route>
+                <Route
+                    path="/login"
+                    element={
+                        <RedirectAuthenticatedUser>
+                            <LoginPage />
+                        </RedirectAuthenticatedUser>
+                    }
+                ></Route>
+                <Route
+                    path="/verify-email"
+                    element={<EmailVerificationPage />}
+                ></Route>
             </Routes>
             <Toaster></Toaster>
         </div>
--- client/src/store/authStore.js
+++ client/src/store/authStore.js
@@ -57,4 +57,23 @@ export const useAuthStore = create((set) => ({
             throw error;
         }
     },
+
+    checkAuth: async () => {
+        set({ isCheckingAuth: true, error: null });
+        try {
+            const response = await axios.get(`${API_URL}/check-auth`);
+            set({
+                user: response.data.user,
+                isAuthenticated: true,
+                isCheckingAuth: false,
+            });
+        } catch (error) {
+            set({
+                error: null,
+                isAuthenticated: false,
+                isCheckingAuth: false,
+            });
+            throw error;
+        }
+    }
 }));
