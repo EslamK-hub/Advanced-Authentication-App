99a3f485fa4a8ee66ed12f892375a560b4c083d9
Eslam Kamal <ek191744@gmail.com>
2025-05-09 20:43:57 +0300

    Fix deployment issue

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import path from "path";
 
 import { connectDB } from "./db/connectDB.js";
 
@@ -11,7 +10,6 @@ import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 
 const app = express();
-const __dirname = path.resolve();
 
 app.use(cors({
     origin: process.env.CLIENT_URL,
@@ -22,14 +20,6 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/client/dist")));
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
-    });
-}
 
 const Port = process.env.PORT || 5000;
 app.listen(Port, () => {
