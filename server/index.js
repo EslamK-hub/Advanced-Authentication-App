bb65503d15233129bee202e03e81a8948d5914bc
Eslam Kamal <ek191744@gmail.com>
2025-05-11 19:50:56 +0300

    The project is ready for production deployment

--- server/index.js
+++ server/index.js
@@ -1,20 +1,34 @@
 import express from "express";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
-import { connectDB } from "./db/connectDB.js";
 
 import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("MongoDB connected"))
+    .catch((error) => console.error(error));
+
 const app = express();
 
-app.use(cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-}))
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL,
+        methods: ["GET", "POST", "DELETE", "PUT"],
+        allowedHeaders: [
+            "Content-Type",
+            "Authorization",
+            "Cache-Control",
+            "Expires",
+            "Pragma",
+        ],
+        credentials: true,
+    })
+);
 
 app.use(express.json());
 app.use(cookieParser());
@@ -23,6 +37,5 @@ app.use("/api/auth", authRoutes);
 
 const Port = process.env.PORT || 5000;
 app.listen(Port, () => {
-    connectDB();
     console.log(`Server is running on port ${Port}`);
 });
